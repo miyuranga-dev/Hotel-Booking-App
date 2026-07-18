1d4d7ebc97e2b91f879babb6368e18e1942f5d21
Prabodana Miyuranga <150063011+miyuranga-dev@users.noreply.github.com>
2026-07-18 12:39:17 +0530

    Simplify mongoose connection options
    Refactor database connection logic to simplify options.

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // 30 seconds timeout
-    });
-    console.log("✅ Database Connected");
-  } catch (error) {
-    console.error("❌ Database Connection Failed:", error.message);
-    process.exit(1); // Stop server if DB connection fails
-  }
+    try {
+        await mongoose.connect(
+            `${process.env.MONGODB_URI}/hotel-booking`,
+            {
+                serverSelectionTimeoutMS: 30000,
+            }
+        );
+
+        console.log("✅ Database Connected");
+
+    } catch (error) {
+        console.error("❌ Database Connection Failed:", error.message);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
