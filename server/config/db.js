ccc4342d1f228960e49e4a023f8db3f368f64961
Prabodana Miyuranga <150063011+miyuranga-dev@users.noreply.github.com>
2026-07-18 13:19:28 +0530

    Refactor MongoDB connection URI handling

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(
-            `${process.env.MONGODB_URI}/hotel-booking`,
-            {
-                serverSelectionTimeoutMS: 30000,
-            }
-        );
+        console.log("Mongo URI:", process.env.MONGODB_URI);
+
+        await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 30000,
+        });
 
         console.log("✅ Database Connected");
 
